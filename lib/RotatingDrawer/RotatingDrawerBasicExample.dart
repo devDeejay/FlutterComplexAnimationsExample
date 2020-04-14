e725368b9dc2e315df611a497410273ef4be665a
Dhananjay Trivedi <dhananjay.trivedi@softwaysolutions.com>
2020-04-14 11:51:28 +0530

    Adding translation animation

--- lib/RotatingDrawer/RotatingDrawerBasicExample.dart
+++ lib/RotatingDrawer/RotatingDrawerBasicExample.dart
@@ -1,111 +1,183 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
-class RotatingDrawerBasicExample extends StatefulWidget {
+class CustomGuitarDrawer extends StatefulWidget {
+  final Widget child;
+
+  const CustomGuitarDrawer({Key key, this.child}) : super(key: key);
+
+  static CustomGuitarDrawerState of(BuildContext context) =>
+      context.findAncestorStateOfType<CustomGuitarDrawerState>();
+
   @override
-  _RotatingDrawerBasicExampleState createState() =>
-      _RotatingDrawerBasicExampleState();
+  CustomGuitarDrawerState createState() => new CustomGuitarDrawerState();
 }
 
-class _RotatingDrawerBasicExampleState extends State<RotatingDrawerBasicExample>
+class CustomGuitarDrawerState extends State<CustomGuitarDrawer>
     with SingleTickerProviderStateMixin {
   AnimationController animationController;
-  bool _canBeDragged = true;
-  int minDragStartEdge = 0;
-  int maxDragStartEdge = 1;
+  bool _canBeDragged = false;
+  final double maxSlide = 300.0;
 
-  final double maxSlide = 225;
-
-  var myDrawer = Container(
-    color: Colors.blue,
-  );
-
-  var myChild = Container(
-    color: Colors.greenAccent,
-  );
-
-  void toggle() {
-    animationController.isDismissed
-        ? animationController.forward()
-        : animationController.reverse();
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
   }
 
-  void onDragStart(DragStartDetails details) {
-    /// To drag manually, we start sliding our finger
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
-    bool isDragOpenFromLeft = animationController.isDismissed &&
-        details.globalPosition.dx < minDragStartEdge;
+  void toggle() => animationController.isDismissed
+      ? animationController.forward()
+      : animationController.reverse();
 
-    bool isDragCloseFromRight = animationController.isCompleted &&
-        details.globalPosition.dy > maxDragStartEdge;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: _onDragStart,
+      onHorizontalDragUpdate: _onDragUpdate,
+      onHorizontalDragEnd: _onDragEnd,
+      behavior: HitTestBehavior.translucent,
+      onTap: toggle,
+      child: AnimatedBuilder(
+        animation: animationController,
+        builder: (context, _) {
+          return Material(
+            color: Colors.blueGrey,
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(maxSlide * (animationController.value - 1), 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(math.pi / 2 * (1 - animationController.value)),
+                    alignment: Alignment.centerRight,
+                    child: MyDrawer(),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(maxSlide * animationController.value, 0),
+                  child: Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(-math.pi * animationController.value / 2),
+                    alignment: Alignment.centerLeft,
+                    child: widget.child,
+                  ),
+                ),
+                Positioned(
+                  top: 4.0 + MediaQuery.of(context).padding.top,
+                  left: 4.0 + animationController.value * maxSlide,
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: toggle,
+                    color: Colors.white,
+                  ),
+                ),
+                Positioned(
+                  top: 16.0 + MediaQuery.of(context).padding.top,
+                  left: animationController.value *
+                      MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'Hello Flutter Europe',
+                    style: Theme.of(context).primaryTextTheme.title,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = animationController.isDismissed;
+    bool isDragCloseFromRight = animationController.isCompleted;
     _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
   }
 
-  void onDragUpdate(DragUpdateDetails details) {
-    /// If we are dragging manually,
-    /// We have to update our animation controller
-    /// Whenever the animation controller value changes,
-    /// The widgets inside AnimatedBuilder() rebuild.
-    /// Even though we are not calling setState() we are rebuilding UI
-
+  void _onDragUpdate(DragUpdateDetails details) {
     if (_canBeDragged) {
       double delta = details.primaryDelta / maxSlide;
       animationController.value += delta;
     }
   }
 
-  void onDragEnd(DragEndDetails details) {
+  void _onDragEnd(DragEndDetails details) {
+    //I have no idea what it means, copied from Drawer
+    double _kMinFlingVelocity = 365.0;
+
     if (animationController.isDismissed || animationController.isCompleted) {
       return;
     }
-    if (details.velocity.pixelsPerSecond.dx.abs() >= 365) {
+    if (details.velocity.pixelsPerSecond.dx.abs() >= _kMinFlingVelocity) {
       double visualVelocity = details.velocity.pixelsPerSecond.dx /
           MediaQuery.of(context).size.width;
+
       animationController.fling(velocity: visualVelocity);
     } else if (animationController.value < 0.5) {
-      closeDrawer();
+      animationController.reverse();
     } else {
-      openDrawer();
+      animationController.forward();
     }
   }
+}
 
-  void closeDrawer() {}
-
-  void openDrawer() {}
-
-  @override
-  void initState() {
-    super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 250));
-  }
-
+class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onHorizontalDragStart: onDragStart,
-      onHorizontalDragUpdate: onDragUpdate,
-      onHorizontalDragEnd: onDragEnd,
-      onTap: toggle,
-      child: AnimatedBuilder(
-        animation: animationController,
-        builder: (context, _) {
-          double slide = maxSlide * animationController.value;
-          double scale = 1 - (animationController.value * 0.3);
-          return Stack(
-            children: <Widget>[
-              myDrawer,
-              Transform(
-                transform: Matrix4.identity()
-                  ..translate(slide)
-                  ..scale(scale),
-                alignment: Alignment.centerLeft,
-                child: myChild,
-              )
-            ],
-          );
-        },
+    return SizedBox(
+      width: 300,
+      height: double.infinity,
+      child: Material(
+        color: Colors.blue,
+        child: SafeArea(
+          child: Theme(
+            data: ThemeData(brightness: Brightness.dark),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset(
+                  'assets/flutter_europe_white.png',
+                  width: 200,
+                ),
+                ListTile(
+                  leading: Icon(Icons.new_releases),
+                  title: Text('News'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text('Favourites'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.map),
+                  title: Text('Map'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Profile'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
